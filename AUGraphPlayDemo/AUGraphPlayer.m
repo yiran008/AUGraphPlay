09f75afd9e702293e9d14465744307aeb574c02d
liumiao <liumuao1988@gmail.com>
2014-11-17 14:51:42 +0800

    change value range for some parameter.

--- AUGraphPlayDemo/AUGraphPlayer.m
+++ AUGraphPlayDemo/AUGraphPlayer.m
@@ -39,9 +39,75 @@ static OSStatus	performRender (void                         *inRefCon,
     self = [super init];
     if (self)
     {
+        //[self initializeAudioSession];
         [self initializeAUGraph];
     }
     return self;
+}
+-(void)initializeAudioSession
+{
+    NSError *error = nil;
+    
+    // Configure the audio session
+    AVAudioSession *sessionInstance = [AVAudioSession sharedInstance];
+    
+    // our default category -- we change this for conversion and playback appropriately
+    [sessionInstance setCategory:AVAudioSessionCategoryPlayback error:&error];
+    XThrowIfError(error.code, "couldn't set audio category");
+    
+    NSTimeInterval bufferDuration = .005;
+    [sessionInstance setPreferredIOBufferDuration:bufferDuration error:&error];
+    XThrowIfError(error.code, "couldn't set IOBufferDuration");
+    
+    double hwSampleRate = 44100.0;
+    [sessionInstance setPreferredSampleRate:hwSampleRate error:&error];
+    XThrowIfError(error.code, "couldn't set preferred sample rate");
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(handleRouteChange:)
+                                                 name:AVAudioSessionRouteChangeNotification
+                                               object:sessionInstance];
+    // activate the audio session
+    [sessionInstance setActive:YES error:&error];
+    XThrowIfError(error.code, "couldn't set audio session active\n");
+    
+    // just print out some info
+    printf("Current IOBufferDuration: %fms\n", sessionInstance.IOBufferDuration * 1000);
+    printf("Hardware Sample Rate: %.1fHz\n", sessionInstance.sampleRate);
+    printf("Current Hardware Output Latency: %fms\n", sessionInstance.outputLatency * 1000);
+
+}
+- (void)handleRouteChange:(NSNotification *)notification
+{
+    UInt8 reasonValue = [[notification.userInfo valueForKey:AVAudioSessionRouteChangeReasonKey] intValue];
+    AVAudioSessionRouteDescription *routeDescription = [notification.userInfo valueForKey:AVAudioSessionRouteChangePreviousRouteKey];
+    
+    NSLog(@"Route change:");
+    switch (reasonValue) {
+        case AVAudioSessionRouteChangeReasonNewDeviceAvailable:
+            NSLog(@"     NewDeviceAvailable");
+            break;
+        case AVAudioSessionRouteChangeReasonOldDeviceUnavailable:
+            NSLog(@"     OldDeviceUnavailable");
+            break;
+        case AVAudioSessionRouteChangeReasonCategoryChange:
+            NSLog(@"     CategoryChange");
+            NSLog(@" New Category: %@", [[AVAudioSession sharedInstance] category]);
+            break;
+        case AVAudioSessionRouteChangeReasonOverride:
+            NSLog(@"     Override");
+            break;
+        case AVAudioSessionRouteChangeReasonWakeFromSleep:
+            NSLog(@"     WakeFromSleep");
+            break;
+        case AVAudioSessionRouteChangeReasonNoSuitableRouteForCategory:
+            NSLog(@"     NoSuitableRouteForCategory");
+            break;
+        default:
+            NSLog(@"     ReasonUnknown");
+    }
+    
+    NSLog(@"Previous route:\n");
+    NSLog(@"%@", routeDescription);
 }
 
 -(void)initializeAUGraph
@@ -147,6 +213,10 @@ static OSStatus	performRender (void                         *inRefCon,
 -(void)dealloc
 {
     DisposeAUGraph(mGraph);
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:AVAudioSessionRouteChangeNotification
+                                                  object:[AVAudioSession sharedInstance]];
+
 }
 
 @end
